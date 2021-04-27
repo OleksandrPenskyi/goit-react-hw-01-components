302dd665864b7790a10c4b301cc2916ec5552ec1
Krytoryni <suchgoodidea@gmail.com>
2021-04-27 23:38:30 +0300

    fix destructurisation at profile.js

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,16 +4,13 @@ import Stats from './Stats';
 
 const Profile = ({ user }) => {
   const { name, tag, location, avatar, stats } = user;
+  const { followers, views, likes } = stats;
 
   return (
     <>
       <Description name={name} tag={tag} location={location} avatar={avatar} />
 
-      <Stats
-        followers={stats.followers}
-        views={stats.views}
-        likes={stats.likes}
-      />
+      <Stats followers={followers} views={views} likes={likes} />
     </>
   );
 };
